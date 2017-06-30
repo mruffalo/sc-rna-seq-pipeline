173ec2cc04b31a3e4edbd3bee25b8de7f410937d
Matt Ruffalo <matt.ruffalo@gmail.com>
2017-06-30 12:09:53 -0400

    parse_gene_intervals.py: tweak RPKM calculation, save summary data

--- parse_gene_intervals.py
+++ parse_gene_intervals.py
@@ -5,18 +5,21 @@ from pathlib import Path
 import pandas as pd
 
 from build_tree import intervals_by_gene, gene_length, trees
-from utils import DATA_PATH, ensure_dir, replace_extension
+from utils import DATA_PATH, append_to_filename, ensure_dir, replace_extension
 
 RPKM_DATA_PATH = DATA_PATH / 'rpkm'
 ensure_dir(RPKM_DATA_PATH)
 
 def main(sam_path: Path):
+    base_csv_path = replace_extension(sam_path, 'csv')
+
     # Read the SAM file of an alignment.
     read_counts = pd.Series(0, index=sorted(intervals_by_gene))
     reads_mapped_to_genes = 0
     reads_aligned = 0
     reads_total = 0
 
+    print('Reading', sam_path)
     with open(sam_path) as f:
         # Read each line of the SAM file.
         for line in f:
@@ -42,7 +45,7 @@ def main(sam_path: Path):
             # Get the gene id at a certain point if there is any.
             gene_ids = trees[chrom][start:end]
 
-            # Reads should never map to multiple genes, but it's still better to be
+            # Reads shouldn't map to multiple genes, but it's still better to be
             # safe with this and not count reads multiple times if this happens
             if gene_ids:
                 reads_mapped_to_genes += 1
@@ -50,36 +53,36 @@ def main(sam_path: Path):
             for gene_id in gene_ids:
                 read_counts.loc[gene_id.data] += 1
 
-    rpkm_csv_path = replace_extension(sam_path, 'csv')
 
-    # Count the reads mapped to the genome and calculate the RPKM of each gene.
-    with open(rpkm_csv_path, 'w') as output:
-        gene_count = 0
+    rpkm = (read_counts * 1000000) / (reads_total * gene_length)
+    rpkm_csv_path = append_to_filename(base_csv_path, '_rpkm')
+    print('Saving RPKM values to', rpkm_csv_path)
+    rpkm.to_csv(rpkm_csv_path)
 
-        # The unit of total reads count would be million.
-        reads_total /= 1000000
+    gene_count = (read_counts > 0).sum()
 
-        for gene_id, interval_data in intervals_by_gene.items():
-            reads_num = read_counts.loc[gene_id]
-            length = gene_length.loc[gene_id]
-            if reads_num != 0:
-                # Calculate the RPKM of genes in that cell.
-                rpkm = reads_num / (reads_total * length)
-                row = gene_id + '\t' + str(rpkm) + '\n'
-                output.write(row.encode())
-                gene_count += 1
-        # combined = pd.concat([gene_counts, build_tree.gene_length], axis=1)
-        # combined.to_csv('/home/zhilinh/output')
-        print('Reads (+1000bp): ' + str(reads_mapped_to_genes))
-        print("Genes (+1000bp): " + str(gene_count))
-        print('Total: ' + str(reads_total))
-        print()
+    print('Reads (+1000bp):', reads_mapped_to_genes)
+    print('Genes (+1000bp):', gene_count)
+    print('Total:', reads_total)
+
+    summary_data = pd.Series(
+        {
+            'read_count': reads_total,
+            'reads_aligned': reads_aligned,
+            'mapped_to_genes': reads_mapped_to_genes,
+            'genes_with_reads': gene_count,
+        }
+    )
+    summary_csv_path = append_to_filename(base_csv_path, '_summary')
+    print('Saving summary data to', summary_csv_path)
+    summary_data.to_csv(summary_csv_path)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description=__doc__,
-        formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument(
-        'geoid', default='', help='GEO_id.')
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+    )
+    parser.add_argument('sam_path', type=Path, help='Path to SAM file')
     args = parser.parse_args()
-    main(args.geoid)
+
+    main(args.sam_path)
