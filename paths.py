9220357163507a574110b75129fe5ad0a74f2494
Matt Ruffalo <matt.ruffalo@gmail.com>
2017-07-05 12:58:48 -0400

    paths.py: use private import name

--- paths.py
+++ paths.py
@@ -1,14 +1,16 @@
-from pathlib import Path
+from pathlib import Path as _Path
 
 # Most of the values here will be used in string formatting operations to
 # build command lists for the `subprocess` module, so it doesn't really
 # matter whether these values are of type `str` or `Path`. Might be useful
 # to use `Path` objects in overrides though, for things like `Path.expanduser`.
 
-FASTQ_DUMP_PATH = Path('fastq-dump')
-HISAT2_PATH = Path('hisat2')
+FASTQ_DUMP_PATH = _Path('fastq-dump')
+HISAT2_PATH = _Path('hisat2')
 # Not an actual file on disk; this is the "base" name
-REFERENCE_INDEX_PATH = Path('~/data/hisat2-indexes/mm10-splice-sites/mm10').expanduser()
+REFERENCE_INDEX_PATH = _Path('~/data/hisat2-indexes/mm10-splice-sites/mm10').expanduser()
+
+del _Path
 
 # Keep this as the last section of this file:
 try:
