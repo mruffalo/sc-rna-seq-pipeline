1ab70bdaa47c321bd813b2766d318be24231de75
Matt Ruffalo <matt.ruffalo@gmail.com>
2018-07-05 18:13:11 -0400

    process_sra_file.py: save data

--- process_sra_file.py
+++ process_sra_file.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 import argparse
 from pathlib import Path
+from pprint import pprint
+
+from data_path_utils import append_to_filename
+import pandas as pd
 
 from alignment import process_sra_file
 
@@ -17,3 +21,11 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     rpkm, summary = process_sra_file(args.sam_path, args.subprocesses)
+    print('Summary data:')
+    pprint(summary)
+
+    rpkm_path = append_to_filename(args.sra_path.with_suffix('.hdf5'), '_rpkm')
+    print('Saving RPKM to', rpkm_path)
+    with pd.HDFStore(rpkm_path) as store:
+        d = pd.DataFrame({args.sra_path.stem: rpkm})
+        store['rpkm'] = d
